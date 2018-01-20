dd223da98b72d15b0c8a7acea5e8148b593b5b26
David Fuentes <fuentesdt@gmail.com>
2018-01-20 14:12:38 -0600

    WIP: organize data`

---
+++ Code/organizeire.py
@@ -0,0 +1,34 @@
+import MySQLdb
+""" Organize IRE Data
+"""
+
+sqldb = MySQLdb.connect(host="scrdep2.mdanderson.org",    # your host, usually localhost
+                        read_default_file="~/.my.cnf",    # username password
+                        db="DFElectroporation")        # name of the data base
+sqlcur = sqldb.cursor()
+# :set syntax=sql
+querydata= """ 
+select concat(concat_ws('/',replace(rf.mrn,' ','_'),REPLACE(rf.StudyDate, '-', ''),rf.StudyUID,rf.seriesuid),'/CT.',rf.imageuid,'.annotationSignature.nii.gz') uid from DFElectroporation.metadata rf where rf.studyuid not like "%control%";
+"""
+sqlcur.execute(querydata)
+queryNames= [description[0] for description in sqlcur.description]
+datalist = [dict(zip(queryNames,x)) for x in sqlcur]
+print queryNames #, queryDict
+
+for iddata in datalist:
+   uidinfo =  iddata['uid'].split('/')
+   datadir = uidinfo [0]
+   localdir = 'spinestudy/' + '/'.join(uidinfo[:-1]) + '/' + uidinfo[-1].replace('.annotationSignature.nii.gz','')
+   seriesdir = "/FUS4/IPVL_research_anno/" + "/".join(uidinfo [:-1])
+   imageuid = uidinfo [4].replace('.annotationSignature.nii.gz','')
+   imagenumber = int(imageuid.split('.')[-1])
+   print datadir,localdir, seriesdir ,imageuid ,imagenumber
+   filelist = []
+   for sliceid in  range(imagenumber-5,imagenumber+5) :
+       slicefile = seriesdir + '/' + '.'.join(imageuid.split('.')[:-1]) + '.%d' % sliceid
+       filelist.append( slicefile)
+   createlocaldir = 'mkdir -p %s; cp %s %s' %(localdir,' '.join(filelist),localdir)
+   print createlocaldir 
+   copyannotation = "c3d anat.nii.gz /FUS4/IPVL_research_anno/%s -pad 0x0x5vox 0x0x4vox  0 -copy-transform -info -type uchar -o myanno.nii.gz" % iddata['uid']
+   print copyannotation 
+
