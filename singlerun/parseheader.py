8cf6afc29c83aef6ee4831b57e9b5855580f0c41
David Fuentes <fuentesdt@gmail.com>
2025-04-18 14:48:44 -0500

    WIP:

--- singlerun/parseheader.py
+++ singlerun/parseheader.py
@@ -3,12 +3,12 @@ import nrrd
 
 fileList = ['dog_1_final_needle_center.seg.nrrd',  'Dog 2 Final_4_8_25.seg.nrrd','dog3_final_segmentation_corrected.seg.nrrd',]
 
+labeldictionary = {}
 # Read the NRRD file
-for myfile in fileList:
+for iddog,myfile in enumerate(fileList):
   print("%s@########################################################### " %myfile )
-  data, header = nrrd.read('dog3_final_segmentation_corrected.seg.nrrd')
-  
-  labeldictionary = {}
+  data, header = nrrd.read(myfile)
+  labeldictionary[iddog+1] = {}
   
   # Access header information, including label fields
   if 'Segment0_Name' in header:
@@ -17,10 +17,24 @@ for myfile in fileList:
           segment_name = header[f'Segment{i}_Name']
           segment_label_value = header[f'Segment{i}_LabelValue']
           print(f"Segment {i}: Name = {segment_name}, Label Value = {segment_label_value}")
-          labeldictionary[segment_name ] = segment_label_value 
+          labeldictionary[iddog+1][segment_name ] = segment_label_value 
           i += 1
   else:
       print("No label field found in the NRRD header.")
 
-print("tissue_replace = %s 1 %s 2 %s 3 %s 3 %s 3 %s 4 %s 5 %s 6  %s 6 %s 6" % (labeldictionary['L5_muscle'], labeldictionary['L5_bone'], labeldictionary['L5_needle'], labeldictionary['L5_needle_tip'], labeldictionary['L5_needle_bottom'], labeldictionary['L5_bone_tumor'], labeldictionary['CSF'], labeldictionary['L5_cord'], labeldictionary['L5_root_left'], labeldictionary['L5_nerve_root_right']) )
+print("dog1L1.ini: tissue_replace = %s 1 %s 2 %s 3 %s 3 %s 3 %s 4 %s 5 %s 6  %s 6 %s 6" % (labeldictionary[3]['L5_muscle'], labeldictionary[3]['L5_bone'], labeldictionary[3]['L5_needle'], labeldictionary[3]['L5_needle_tip'], labeldictionary[3]['L5_needle_bottom'], labeldictionary[3]['L5_bone_tumor'], labeldictionary[3]['CSF'], labeldictionary[3]['L5_cord'], labeldictionary[3]['L5_root_left'], labeldictionary[3]['L5_nerve_root_right']) )
+print("dog1L3.ini: tissue_replace = %s 1 %s 2 %s 3 %s 3 %s 3 %s 4 %s 5 %s 6  %s 6 %s 6" % (labeldictionary[3]['L5_muscle'], labeldictionary[3]['L5_bone'], labeldictionary[3]['L5_needle'], labeldictionary[3]['L5_needle_tip'], labeldictionary[3]['L5_needle_bottom'], labeldictionary[3]['L5_bone_tumor'], labeldictionary[3]['CSF'], labeldictionary[3]['L5_cord'], labeldictionary[3]['L5_root_left'], labeldictionary[3]['L5_nerve_root_right']) )
+print("dog1L5.ini: tissue_replace = %s 1 %s 2 %s 3 %s 3 %s 3 %s 4 %s 5 %s 6  %s 6 %s 6" % (labeldictionary[3]['L5_muscle'], labeldictionary[3]['L5_bone'], labeldictionary[3]['L5_needle'], labeldictionary[3]['L5_needle_tip'], labeldictionary[3]['L5_needle_bottom'], labeldictionary[3]['L5_bone_tumor'], labeldictionary[3]['CSF'], labeldictionary[3]['L5_cord'], labeldictionary[3]['L5_root_left'], labeldictionary[3]['L5_nerve_root_right']) )
+print("dog1T11.ini: tissue_replace = %s 1 %s 2 %s 3 %s 3 %s 3 %s 4 %s 5 %s 6  %s 6 %s 6" % (labeldictionary[3]['L5_muscle'], labeldictionary[3]['L5_bone'], labeldictionary[3]['L5_needle'], labeldictionary[3]['L5_needle_tip'], labeldictionary[3]['L5_needle_bottom'], labeldictionary[3]['L5_bone_tumor'], labeldictionary[3]['CSF'], labeldictionary[3]['L5_cord'], labeldictionary[3]['L5_root_left'], labeldictionary[3]['L5_nerve_root_right']) )
+print("dog1T12.ini: tissue_replace = %s 1 %s 2 %s 3 %s 3 %s 3 %s 4 %s 5 %s 6  %s 6 %s 6" % (labeldictionary[3]['L5_muscle'], labeldictionary[3]['L5_bone'], labeldictionary[3]['L5_needle'], labeldictionary[3]['L5_needle_tip'], labeldictionary[3]['L5_needle_bottom'], labeldictionary[3]['L5_bone_tumor'], labeldictionary[3]['CSF'], labeldictionary[3]['L5_cord'], labeldictionary[3]['L5_root_left'], labeldictionary[3]['L5_nerve_root_right']) )
+
+
+print("dog2L1.ini: tissue_replace = %s 1 %s 2 %s 3 %s 3 %s 3 %s 4 %s 5 %s 6  %s 6 %s 6" % (labeldictionary[3]['L5_muscle'], labeldictionary[3]['L5_bone'], labeldictionary[3]['L5_needle'], labeldictionary[3]['L5_needle_tip'], labeldictionary[3]['L5_needle_bottom'], labeldictionary[3]['L5_bone_tumor'], labeldictionary[3]['CSF'], labeldictionary[3]['L5_cord'], labeldictionary[3]['L5_root_left'], labeldictionary[3]['L5_nerve_root_right']) )
+print("dog2L3.ini: tissue_replace = %s 1 %s 2 %s 3 %s 3 %s 3 %s 4 %s 5 %s 6  %s 6 %s 6" % (labeldictionary[3]['L5_muscle'], labeldictionary[3]['L5_bone'], labeldictionary[3]['L5_needle'], labeldictionary[3]['L5_needle_tip'], labeldictionary[3]['L5_needle_bottom'], labeldictionary[3]['L5_bone_tumor'], labeldictionary[3]['CSF'], labeldictionary[3]['L5_cord'], labeldictionary[3]['L5_root_left'], labeldictionary[3]['L5_nerve_root_right']) )
+print("dog2L5.ini: tissue_replace = %s 1 %s 2 %s 3 %s 3 %s 3 %s 4 %s 5 %s 6  %s 6 %s 6" % (labeldictionary[3]['L5_muscle'], labeldictionary[3]['L5_bone'], labeldictionary[3]['L5_needle'], labeldictionary[3]['L5_needle_tip'], labeldictionary[3]['L5_needle_bottom'], labeldictionary[3]['L5_bone_tumor'], labeldictionary[3]['CSF'], labeldictionary[3]['L5_cord'], labeldictionary[3]['L5_root_left'], labeldictionary[3]['L5_nerve_root_right']) )
+print("dog2T12.ini: tissue_replace = %s 1 %s 2 %s 3 %s 3 %s 3 %s 4 %s 5 %s 6  %s 6 %s 6" % (labeldictionary[3]['L5_muscle'], labeldictionary[3]['L5_bone'], labeldictionary[3]['L5_needle'], labeldictionary[3]['L5_needle_tip'], labeldictionary[3]['L5_needle_bottom'], labeldictionary[3]['L5_bone_tumor'], labeldictionary[3]['CSF'], labeldictionary[3]['L5_cord'], labeldictionary[3]['L5_root_left'], labeldictionary[3]['L5_nerve_root_right']) )
+
+print("dog3L3.ini: tissue_replace = %s 1 %s 2 %s 3 %s 3 %s 3 %s 4 %s 5 %s 6  %s 6 %s 6" % (labeldictionary[3]['L5_muscle'], labeldictionary[3]['L5_bone'], labeldictionary[3]['L5_needle'], labeldictionary[3]['L5_needle_tip'], labeldictionary[3]['L5_needle_bottom'], labeldictionary[3]['L5_bone_tumor'], labeldictionary[3]['CSF'], labeldictionary[3]['L5_cord'], labeldictionary[3]['L5_root_left'], labeldictionary[3]['L5_nerve_root_right']) )
+print("dog3L5.ini: tissue_replace = %s 1 %s 2 %s 3 %s 3 %s 3 %s 4 %s 5 %s 6  %s 6 %s 6" % (labeldictionary[3]['L5_muscle'], labeldictionary[3]['L5_bone'], labeldictionary[3]['L5_needle'], labeldictionary[3]['L5_needle_tip'], labeldictionary[3]['L5_needle_bottom'], labeldictionary[3]['L5_bone_tumor'], labeldictionary[3]['CSF'], labeldictionary[3]['L5_cord'], labeldictionary[3]['L5_root_left'], labeldictionary[3]['L5_nerve_root_right']) )
+print("dog3T12.ini: tissue_replace = %s 1 %s 2 %s 3 %s 3 %s 3 %s 4 %s 5 %s 6  %s 6 %s 6" % (labeldictionary[3]['L5_muscle'], labeldictionary[3]['L5_bone'], labeldictionary[3]['L5_needle'], labeldictionary[3]['L5_needle_tip'], labeldictionary[3]['L5_needle_bottom'], labeldictionary[3]['L5_bone_tumor'], labeldictionary[3]['CSF'], labeldictionary[3]['L5_cord'], labeldictionary[3]['L5_root_left'], labeldictionary[3]['L5_nerve_root_right']) )
 
